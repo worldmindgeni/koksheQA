7bad2765bbc2b5fa79fa11f28297cafb20471df1
worldmindgeni <botvinko540@gmail.com>
2024-10-14 16:49:05 +0500

    upload files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('menu-toggle').onclick = function() {
+    document.getElementById('sidebar').style.display = 'block';
+};
+
+document.getElementById('close-btn').onclick = function() {
+    document.getElementById('sidebar').style.display = 'none';
+};
